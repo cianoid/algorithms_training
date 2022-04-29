db9d6fcf82835f909f67211f48b0f3153d052855
Igor Shatava <cianoid@yandex.ru>
2022-04-29 23:16:49 +0300

    F task completed

--- yandex_contest_23758/F/main.py
+++ yandex_contest_23758/F/main.py
@@ -10,27 +10,34 @@ class StackMax:
         return len(self.stack) == 0
 
     def push(self, x):
-        if self.max is None or x > self.max:
-            self.max = x
-
         self.stack.append(x)
 
     def pop(self):
-        if self.is_empty():
+        try:
+            self.stack.pop()
+        except IndexError:
             print('error')
-            return False
-
-        return self.stack.pop()
 
     def get_max(self):
-        if self.is_empty():
-            print(None)
-            return False
-
-        return self.max
+        try:
+            print(max(self.stack))
+        except ValueError:
+            print('None')
 
 
 if __name__ == '__main__':
+    command_count = int(input())
+
     stack = StackMax()
 
-    print(stack.is_empty())
+    while command_count > 0:
+        command = input().split()
+
+        func_name = command[0]
+
+        if len(command) == 2:
+            getattr(stack, func_name)(int(command[1]))
+        else:
+            getattr(stack, func_name)()
+
+        command_count -= 1
