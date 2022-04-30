df7cb00068d44a8c89b0c40595e60ae5c655dcd9
Igor Shatava <cianoid@yandex.ru>
2022-04-30 22:32:16 +0300

    I task completed

--- yandex_contest_23758/I/main.py
+++ yandex_contest_23758/I/main.py
@@ -1,21 +1,63 @@
 class MyQueueSized:
+    max_size = 0
+    queue_size = 0
+    head = 0
+    tail = 0
+    queue: list
+
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.queue = [None] * max_size
+
     def push(self, x):
-        pass
+        if self.queue_size == self.max_size:
+            print('error')
+            return
+
+        self.queue[self.tail] = x
+        self.queue_size += 1
+        self.tail += 1
+
+        if self.tail == -1:
+            self.tail = self.max_size - 1
+        elif self.tail == self.max_size:
+            self.tail = 0
 
     def pop(self):
-        pass
+        if self.is_empty():
+            print(None)
+            return
+
+        popped = self.queue[self.head]
+        self.head += 1
+        self.queue_size -= 1
+
+        if self.head == -1:
+            self.head = self.max_size - 1
+        elif self.head == self.max_size:
+            self.head = 0
+
+        print(popped)
 
     def peek(self):
-        pass
+        if self.is_empty():
+            print(None)
+            return
+
+        print(self.queue[self.head])
 
     def size(self):
-        pass
+        print(self.queue_size)
+
+    def is_empty(self):
+        return self.queue_size == 0
 
 
 if __name__ == '__main__':
     command_count = int(input())
+    queue_size = int(input())
 
-    stack = MyQueueSized()
+    queue = MyQueueSized(queue_size)
 
     while command_count > 0:
         command = input().split()
@@ -23,8 +65,8 @@ if __name__ == '__main__':
         func_name = command[0]
 
         if len(command) == 2:
-            getattr(stack, func_name)(int(command[1]))
+            getattr(queue, func_name)(int(command[1]))
         else:
-            getattr(stack, func_name)()
+            getattr(queue, func_name)()
 
         command_count -= 1
