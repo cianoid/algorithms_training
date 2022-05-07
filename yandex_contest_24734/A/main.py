ae2dab11154b58278181f8b1ed5fd711acb9598f
Igor Shatava <cianoid@yandex.ru>
2022-05-07 07:17:43 +0300

    contest 24734: task A completed

--- yandex_contest_24734/A/main.py
+++ yandex_contest_24734/A/main.py
@@ -1,13 +1,14 @@
-def gen_binary(n, prefix):
+def gen_binary(n, prefix, max_open_br_count, open_br):
     if n == 0:
         print(prefix)
     else:
-        gen_binary(n - 1, prefix + '0')
-        gen_binary(n - 1, prefix + '1')
+        if open_br < max_open_br_count and n > open_br:
+            gen_binary(n - 1, prefix + '(', max_open_br_count, open_br + 1)
+        if open_br > 0:
+            gen_binary(n - 1, prefix + ')', max_open_br_count, open_br - 1)
 
 
 if __name__ == '__main__':
     variant_count = int(input())
 
-    print(gen_binary(variant_count, ''))
-
+    gen_binary(variant_count * 2, '', variant_count, 0)
