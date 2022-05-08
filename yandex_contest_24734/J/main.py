b8a1aa4b36820ef90ab9bb8a69bb46013633c8ee
Igor Shatava <cianoid@yandex.ru>
2022-05-08 08:33:54 +0300

    contest 24734: task J completed

--- yandex_contest_24734/J/main.py
+++ yandex_contest_24734/J/main.py
@@ -1,4 +1,25 @@
+def bubble_sort(array_len, array):
+    n = 0
+
+    while n <= array_len:
+        swapped = False
+        for i in range(0, array_len):
+            j = i + 1
+            if j == array_len:
+                continue
+
+            if array[i] > array[j]:
+                item_to_swap = array[j]
+                array[j] = array[i]
+                array[i] = item_to_swap
+                swapped = True
+
+        n += 1
+
+        if swapped:
+            print(' '.join(str(elem) for elem in array))
+
 
 if __name__ == '__main__':
-    array_len = int(input())
-    array = [int(x) for x in input().split()]
+    bubble_sort(
+        int(input()), [int(x) for x in input().split()])
