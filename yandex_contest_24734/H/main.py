19008dcad925533c7d85255392f73ce1aa364660
Igor Shatava <cianoid@yandex.ru>
2022-05-08 11:06:41 +0300

    contest 24734: task H completed

--- yandex_contest_24734/H/main.py
+++ yandex_contest_24734/H/main.py
@@ -1,7 +1,21 @@
-def big_number(combination_count, numbers):
-    pass
+def combinations(count, arr, memo, results):
+    for i in range(0, count):
+        cur = [arr.pop(i)]
+
+        if len(arr) == 0:
+            results.append(int(''.join(memo + cur)))
+
+        combinations(count-1, arr, memo + cur, results)
+        arr.insert(i, cur[0])
+
+    return results
+
+
+def big_number(number_count, numbers):
+    big_numbers = combinations(number_count, numbers, [], [])
+
+    return max(big_numbers)
 
 
 if __name__ == '__main__':
-    big_number(
-        int(input()), [int(x) for x in input().split()])
+    print(big_number(int(input()), input().split()))
--- yandex_contest_24734/J/main.py
+++ yandex_contest_24734/J/main.py
@@ -1,6 +1,7 @@
 def bubble_sort(array_len, array):
     n = 0
 
+    swap_count = 0
     while n <= array_len:
         swapped = False
         for i in range(0, array_len):
@@ -13,11 +14,15 @@ def bubble_sort(array_len, array):
                 array[j] = array[i]
                 array[i] = item_to_swap
                 swapped = True
+                swap_count += 1
 
         n += 1
 
         if swapped:
             print(' '.join(str(elem) for elem in array))
+
+    if swap_count == 0:
+        print(' '.join(str(elem) for elem in array))
 
 
 if __name__ == '__main__':
