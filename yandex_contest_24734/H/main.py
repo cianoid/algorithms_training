7fecf7e6b1ee12cd2a17607b9147e3dbe1961895
Igor Shatava <cianoid@yandex.ru>
2022-05-15 12:19:09 +0300

    contest 24734: task H completed

--- yandex_contest_24734/H/main.py
+++ yandex_contest_24734/H/main.py
@@ -1,33 +1,48 @@
-import time
+def get_bigest_number(el1, el2):
+    len_el1 = len(el1)
+    len_el2 = len(el2)
+
+    if len_el1 == len_el2:
+        return int(el1) >= int(el2)
+    if el1[0] > el2[0]:
+        return True
+    if el1[0] < el2[0]:
+        return False
+
+    oel1 = el1
+    oel2 = el2
+
+    len_delta = abs(len_el1 - len_el2)
+
+    if len_el1 > len_el2:
+        for i in range(len_delta):
+            el2 += el2[0]
+    elif len_el2 > len_el1:
+        for i in range(len_delta):
+            el1 += el1[0]
+
+    if el1 == el2:
+        return int(oel1 + oel2) > int(oel2 + oel1)
+
+    return int(el1) >= int(el2)
 
 
-def combinations(count, arr, memo, lenarr, maximum):
-    for i in range(0, count):
-        if arr[i] is not None:
+def sort_by_number(len_array, array, func):
+    for i in range(1, len_array):
+        item_to_insert = array[i]
+        j = i
 
-            cur = arr.pop(i)
-            lenarr -= 1
+        while j > 0 and func(item_to_insert, array[j - 1]):
+            array[j] = array[j - 1]
+            j -= 1
 
-            if lenarr == 0:
-                x = int(memo + cur)
-                if x > maximum:
-                    maximum = x
+        array[j] = item_to_insert
 
-            maximum = combinations(
-                count - 1, arr, memo + cur, lenarr, maximum)
-            arr.insert(i, cur)
-            lenarr += 1
-
-    return maximum
-
-
-def big_number(number_count, numbers):
-    t1 = time.time()
-    big_numbers = combinations(number_count, numbers, '', number_count, 0)
-    print('Elapsed {}ms'.format(round(1000 * (time.time() - t1))))
-
-    return big_numbers
+    return array
 
 
 if __name__ == '__main__':
-    print(big_number(int(input()), input().split()))
+    n = int(input())
+    data = input().split()
+
+    print(''.join(sort_by_number(n, data, get_bigest_number)))
