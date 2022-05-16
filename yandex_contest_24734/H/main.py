20723e36e04d7b0b4c09a9e126236e0c198cbfd1
cianoid <cianid@yandex.ru>
2022-05-16 12:14:45 +0300

    fix H task

--- yandex_contest_24734/H/main.py
+++ yandex_contest_24734/H/main.py
@@ -1,30 +1,14 @@
 def get_bigest_number(el1, el2):
-    len_el1 = len(el1)
-    len_el2 = len(el2)
-
-    if len_el1 == len_el2:
+    if len(el1) == len(el2):
         return int(el1) >= int(el2)
+
     if el1[0] > el2[0]:
         return True
+
     if el1[0] < el2[0]:
         return False
 
-    oel1 = el1
-    oel2 = el2
-
-    len_delta = abs(len_el1 - len_el2)
-
-    if len_el1 > len_el2:
-        for i in range(len_delta):
-            el2 += el2[0]
-    elif len_el2 > len_el1:
-        for i in range(len_delta):
-            el1 += el1[0]
-
-    if el1 == el2:
-        return int(oel1 + oel2) > int(oel2 + oel1)
-
-    return int(el1) >= int(el2)
+    return int(el1 + el2) > int(el2 + el1)
 
 
 def sort_by_number(len_array, array, func):
