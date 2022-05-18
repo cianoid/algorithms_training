9b81cb0fec66b5c11dacde25791e7fe75d616c21
Igor Shatava <cianoid@yandex.ru>
2022-05-18 08:02:43 +0300

    contest 24734: task K

---
+++ yandex_contest_24734/K/main.py
@@ -0,0 +1,22 @@
+def merge(arr: list, left: int, mid: int, right: int) -> list:
+    pass
+
+
+def merge_sort(arr: list, left: int, right: int) -> None:
+    pass
+
+
+def test():
+    a = [1, 4, 9, 2, 10, 11]
+    b = merge(a, 0, 3, 6)
+    expected = [1, 2, 4, 9, 10, 11]
+    assert b == expected
+
+    c = [1, 4, 2, 10, 1, 2]
+    merge_sort(c, 0, 6)
+    expected = [1, 1, 2, 2, 4, 10]
+    assert c == expected
+
+
+if __name__ == '__main__':
+    test()
