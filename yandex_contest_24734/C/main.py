6f5863715d8eff83ba3586d5f425e08fe180f9fa
Igor Shatava <cianoid@yandex.ru>
2022-05-18 17:22:19 +0300

    contest 24734: task C completed

--- yandex_contest_24734/C/main.py
+++ yandex_contest_24734/C/main.py
@@ -1,9 +1,22 @@
-def get_subsequence(s, t):
-    return False
+def is_subsubsequence(s, t):
+    if s == '':
+        return True
+
+    if t == '':
+        return False
+
+    last_index = -1
+    for letter in s:
+        try:
+            last_index = t.index(letter, last_index + 1)
+        except ValueError:
+            return False
+
+    return True
 
 
 if __name__ == '__main__':
     inp_s = input()
     inp_t = input()
 
-    print(get_subsequence(inp_s, inp_t))
+    print(is_subsubsequence(inp_s, inp_t))
