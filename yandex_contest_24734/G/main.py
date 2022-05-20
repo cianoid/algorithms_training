94fefb2713c5a42dd2de48bdefd302b708e81483
Igor Shatava <cianoid@yandex.ru>
2022-05-20 21:21:18 +0300

    contest 24734: task G completed

--- yandex_contest_24734/G/main.py
+++ yandex_contest_24734/G/main.py
@@ -1,6 +1,14 @@
+def sort_clothes(clothes_colors):
+    elements = {0: 0, 1: 0, 2: 0}
+
+    for element in clothes_colors:
+        elements[element] += 1
+
+    return [0] * elements[0] + [1] * elements[1] + [2] * elements[2]
+
+
 if __name__ == '__main__':
     inp_clothes_count = int(input())
-    inp_clocthes_colors = [int(x) for x in input().split()]
+    inp_clothes_colors = [int(x) for x in input().split()]
 
-
-
+    print(*sort_clothes(inp_clothes_colors))
