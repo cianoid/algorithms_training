82159e61a17b8ba204e0e084932978c9ab27a384
Igor Shatava <cianoid@yandex.ru>
2022-05-20 21:48:24 +0300

    contest 24735: task A

---
+++ yandex_contest_24735/A/main.py
@@ -0,0 +1,15 @@
+# ID посылки:
+
+def broken_search(nums, target) -> int:
+    #  Your code
+    #  “ヽ(´▽｀)ノ”
+    pass
+
+
+def test():
+    arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
+    assert broken_search(arr, 5) == 6
+
+
+if __name__ == '__main__':
+    test()
