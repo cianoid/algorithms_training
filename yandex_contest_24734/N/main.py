3b9cead89d7646a11ab548ec9b68ff6f955dc78a
Igor Shatava <cianoid@yandex.ru>
2022-05-21 10:29:15 +0300

    contest 24734: task N completed

--- yandex_contest_24734/N/main.py
+++ yandex_contest_24734/N/main.py
@@ -1,133 +1,20 @@
-def sort_by_number(len_array, array):
-    for i in range(1, len_array):
-        item_to_insert = array[i]
-        j = i
-
-        while j > 0 and item_to_insert < array[j - 1]:
-            array[j] = array[j - 1]
-            j -= 1
-
-        array[j] = item_to_insert
-
-    return array
-
-
-def bubble_sort(array_len, array):
-    n = 0
-
-    swap_count = 0
-    while n <= array_len:
-        for i in range(0, array_len):
-            j = i + 1
-            if j == array_len:
-                continue
-
-            if array[i] > array[j]:
-                item_to_swap = array[j]
-                array[j] = array[i]
-                array[i] = item_to_swap
-                swap_count += 1
-
-        n += 1
-
-    return array
-
-
-def merge_sort(array):
-    if len(array) == 1:
-        return array
-
-    mid = len(array) // 2
-    left = merge_sort(array[0:mid])
-    right = merge_sort(array[mid:len(array)])
-
-    result = [None] * len(array)
-
-    counter1, counter2, counter3 = 0, 0, 0
-    while counter1 < len(left) and counter2 < len(right):
-        if left[counter1] <= right[counter2]:
-            result[counter3] = left[counter1]
-            counter1 += 1
-        else:
-            result[counter3] = right[counter2]
-            counter2 += 1
-        counter3 += 1
-
-    while counter1 < len(left):
-        result[counter3] = left[counter1]
-        counter1 += 1
-        counter3 += 1
-
-    while counter2 < len(right):
-        result[counter3] = right[counter2]
-        counter2 += 1
-        counter3 += 1
-
-    return result
-
-
-def partition(array, pivot):
-    left = [None] * len(array)
-    center = left.copy()
-    right = left.copy()
-
-    len_left = 0
-    len_center = 0
-    len_right = 0
-
-    for element in array:
-
-        if element < pivot:
-            left[len_left] = element
-            len_left += 1
-        elif element == pivot:
-            center[len_center] = element
-            len_center += 1
-        else:
-            right[len_right] = element
-            len_right += 1
-
-    return left[:len_left], center[:len_center], right[:len_right]
-
-
-def quick_sort(array):
-    if len(array) < 2:
-        return array
-
-    pivot = array[len(array) // 2]
-    left, center, right = partition(array, pivot)
-
-    return quick_sort(left) + center + quick_sort(right)
-
-
-def get_total_gardenbeds(gardenbeds, gardener_count):
-    # gardenbeds = merge_sort(gardenbeds)
-    # gardenbeds = sorted(gardenbeds)
-    gardenbeds = quick_sort(gardenbeds)
-
+def get_total_gardenbeds(gardenbeds):
+    gardenbeds = sorted(gardenbeds)
     garden = []
 
+    start2, end2 = -1, -1
     for start1, end1 in gardenbeds:
-        if len(garden) == 0:
-            garden.append([start1, end1])
+        if start2 == -1:
+            start2, end2 = start1, end1
             continue
 
-        index2 = -1
-        add = False
+        if start1 > end2:
+            garden.append(tuple([start2, end2]))
+            start2, end2 = start1, end1
+        elif end1 > end2:
+            end2 = end1
 
-        for start2, end2 in garden:
-            # print(f'{start2}-{end2}')
-            index2 += 1
-            if start1 <= end2:
-                if end1 > end2:
-                    garden[index2][1] = end1
-                add = False
-                break
-            else:
-                add = True
-
-        if add:
-            garden.append([start1, end1])
+    garden.append(tuple([start2, end2]))
 
     return garden
 
@@ -137,9 +24,9 @@ if __name__ == '__main__':
     inp_gardenbeds = []
 
     for _no_i in range(inp_gardener_count):
-        inp_gardenbeds.append([int(x) for x in input().split()])
+        inp_gardenbeds.append(tuple(int(x) for x in input().split()))
 
-    gd = get_total_gardenbeds(inp_gardenbeds, inp_gardener_count)
+    gd = get_total_gardenbeds(inp_gardenbeds)
 
     for g in gd:
         print(*g)
