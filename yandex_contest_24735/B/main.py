21a3544271bebcbc6330482a412432b655390892
cianoid <cianid@yandex.ru>
2022-05-23 14:54:15 +0300

    contest 24735: task B

--- yandex_contest_24735/B/main.py
+++ yandex_contest_24735/B/main.py
@@ -1,7 +1,44 @@
 # ID посылки:
 
+def get_element_index(element):
+    return tuple([-element[1], element[2], element[0]])
+
+
+def partition(participants, pivot):
+    left = [None] * len(participants)
+    center = left.copy()
+    right = left.copy()
+
+    len_left = 0
+    len_center = 0
+    len_right = 0
+
+    pivot_index = get_element_index(pivot)
+
+    for element in participants:
+        element_index = get_element_index(element)
+
+        if element_index < pivot_index:
+            left[len_left] = element
+            len_left += 1
+        elif element_index > pivot_index:
+            right[len_right] = element
+            len_right += 1
+        else:
+            center[len_center] = element
+            len_center += 1
+
+    return left[:len_left], center[:len_center], right[:len_right]
+
+
 def effective_quick_sort(participants):
-    pass
+    if len(participants) < 2:
+        return participants
+
+    pivot = participants[len(participants) // 2]
+    left, center, right = partition(participants, pivot)
+
+    return effective_quick_sort(left) + center + effective_quick_sort(right)
 
 
 if __name__ == '__main__':
@@ -14,4 +51,5 @@ if __name__ == '__main__':
                 int(param) if param.isdigit() else param
                 for param in input().split()]))
 
-    effective_quick_sort(inp_participants)
+    for participant in effective_quick_sort(inp_participants):
+        print(participant[0])
